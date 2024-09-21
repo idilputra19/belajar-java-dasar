a4c7ac88940076030bfa117d42c49c1ecab45e32
idilputra <94298940+idilputra19@users.noreply.github.com>
2024-09-21 14:27:01 +0700

    new
    new

---
+++ programkedua.java
@@ -0,0 +1,8 @@
+
+public class programkedua {
+public static void main (String[] args) {
+System.out.println("Ini adalah program java pertama kali saya buat");
+System.out.println("Semoga program yang saya buat ini berhasil dijalankan");
+System.out.println("dan semoga ilmu bermanfaat.aamiin");
+}
+}
