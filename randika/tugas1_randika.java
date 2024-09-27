01a15fcc9db04442015cf573de4c904c12811e82
idilputra <94298940+idilputra19@users.noreply.github.com>
2024-09-27 18:58:56 +0700

    tugas bg randika
    tugas bg randika

---
+++ randika/tugas1_randika.java
@@ -0,0 +1,36 @@
+public class tugas1_randika {
+
+    public static void main(String[] args) {
+        // Bagian atas
+        System.out.println("---------------------------------------------------------");
+        System.out.println("======================*BIODATA SINGKAT*=============");
+        System.out.println("---------------------------------------------------------");
+        System.out.println("1. Data Diri-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=");
+        System.out.println("=========================================================");
+        System.out.println("Nama Lengkap        : Randika Delvia");
+        System.out.println("Nama Panggilan      : Randika");
+        System.out.println("Jenis Kelamin       : Laki - Laki");
+        System.out.println("Agama               : Islam");
+        System.out.println("Kewarganegaraan     : Indonesia");
+        System.out.println("Tempat Tgl Lahir    : Bukittinggi , 5 Januari 1990");
+        System.out.println("Umur                : 34 tahun");
+        System.out.println("Tinggi Dan Berat BB : 168 cm / 78 kg");
+        System.out.println("No Hp               : 0852-7220-8709");
+        System.out.println("E-Mail              : randika.delvia.rd@gmail.com");
+        System.out.println("Alamat              : Jl Rohana Kudus No 69");
+        System.out.println("Hobi                : 1.Nonton TV");
+        System.out.println("                      2.Traveling");
+        System.out.println("=========================================================");
+        
+        // Bagian Riwayat Pendidikan
+        System.out.println("2. Riwayat Pendidikan =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=");
+        System.out.println("=========================================================");
+        System.out.println("Taman Kanak- kanak  : Tk Diniyah Putri Padang Panjang");
+        System.out.println("SD                  : Sd N 08 Kebun Sikolos Padang Panjan");
+        System.out.println("SMP                 : SMP N 2 Padang Panjang");
+        System.out.println("SMA                 : Smk Cendana Padang Panjang");
+        System.out.println("Kuliah Diploma      : Politeknik Negeri Padang ");
+        System.out.println("Prodi               : D3 Teknologi Informasi");
+        System.out.println("=========================================================");
+    }
+}
