5d7d66135c191d33a22f3b85d43caf0acd84e3e1
idilputra <94298940+idilputra19@users.noreply.github.com>
2024-10-01 21:21:13 +0700

    TUGAS RAHMI
    TUGAS RAHMI

---
+++ modul2_rahmi/tugas2no2_rahmi.java
@@ -0,0 +1,18 @@
+public class tugas2no2_rahmi {
+    public static void main(String[] args) {
+        String[] buah;
+        buah = new String[8]; // Mengubah ukuran array menjadi 12 untuk 12 buah
+        
+        // Mengisi array dengan nama buah
+        buah[0] = "Apel";
+        buah[1] = "Jeruk";
+        buah[2] = "Pisang";
+        buah[3] = "Mangga";
+        buah[4] = "Anggur";
+        buah[5] = "Semangka";
+        buah[6] = "Pepaya";
+        buah[7] = "Nanas";
+        // Menampilkan salah satu nama buah, misalnya buah pertama
+        System.out.println("Nama Buah ke-1: " + buah[0]);
+    }
+}
